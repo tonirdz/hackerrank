1c17850dc6b23b74b2b7eb15e19c0c0645fbe818
Toni Rdz <33283061+tonirdz@users.noreply.github.com>
2025-07-17 10:59:31 +0200

    Setup python folder and solve staircase first problem

---
+++ python/staircase.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+
+def staircase(n):
+    # Write your code her
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "#" * (i))
+
+
+if __name__ == "__main__":
+    n = int(input().strip())
+
+    staircase(n)
